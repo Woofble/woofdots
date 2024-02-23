4f245950da84d2299f3141f0ffef69c84205eac0
Woofble <47680027+Woofble@users.noreply.github.com>
2024-02-22 20:10:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo pacman -Sy
 
 # Install required packages...
-sudo pacman -S nano vim git unzip less wget curl neofetch htop
+sudo pacman -S nano vim git unzip less wget curl neofetch htop kitty
 
 # Install yay...
 git clone https://aur.archlinux.org/yay.git
