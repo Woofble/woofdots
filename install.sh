0fb09e46458126bb2d41df339cdc1e2f83fdd78f
Woofble <47680027+Woofble@users.noreply.github.com>
2024-02-22 21:22:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,15 @@ makepkg -si
 cd ..
 sudo rm -r yay
 
+# Install Ly...
+git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly
+make
+sudo make install installsystemd
+systemctl enable ly.service
+cd ..
+sudo rm -r ly
+
 # Install Hyprland...
 yay -Syu --devel
 yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
@@ -28,15 +37,6 @@ cp -r
 
 cd ..
 sudo rm -r Hyprland
-
-# Install Ly...
-git clone --recurse-submodules https://github.com/fairyglade/ly
-cd ly
-make
-sudo make install installsystemd
-systemctl enable ly.service
-cd ..
-sudo rm -r ly
 
 # Install optional packages...
 sudo pacman -S firefox
