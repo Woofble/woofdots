dce8271fb2b06edb536f35cbc512231c32f47a17
Woofble <47680027+Woofble@users.noreply.github.com>
2024-02-23 00:49:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ sudo rm -r ly
 
 # Install Hyprland...
 yay -Syu --devel
-yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 make all
