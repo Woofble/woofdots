459a404efd6107bb0bc56ec25f5ec1e96c879bf5
Woofble <47680027+Woofble@users.noreply.github.com>
2024-02-22 19:08:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,17 @@
 sudo pacman -Sy
 
 # Install required packages...
-sudo pacman -S nano vim git unzip less wget curl neofetch htop
+sudo pacman -S nano vim git unzip less wget curl neofetch htop base-devel
+
+# Install yay...
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo rm -r yay
 
 # Install Hyprland...
+yay -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 make all && sudo make install
